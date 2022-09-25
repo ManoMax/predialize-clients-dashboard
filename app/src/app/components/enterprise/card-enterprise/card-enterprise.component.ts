b81ca17c023fa188834825eace4c30f7c0684149
ManoMax <gabrielmaxcontato@gmail.com>
2022-09-25 19:25:09 -0300

    refactor: mudança de estrutura do componente, para atender a visualização na página de listagem e na página de cliente

--- app/src/app/components/enterprise/card-enterprise/card-enterprise.component.ts
+++ app/src/app/components/enterprise/card-enterprise/card-enterprise.component.ts
@@ -9,18 +9,28 @@ export class CardEnterpriseComponent implements OnInit, AfterViewInit {
 
   @Input()
   data?: any;
+  @Input()
+  page_client_view: boolean;
 
   @ViewChild('imgEnterprise', null) input: ElementRef<HTMLImageElement>;
+  @ViewChild('info_list', null) input2: ElementRef<HTMLImageElement>;
+  @ViewChild('info_list2', null) input3: ElementRef<HTMLImageElement>;
 
   constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
+    
   }
 
   ngAfterViewInit() {
-    let enterprise = this.data.enterprise;
+    let enterprise = this.data;
     if (enterprise.image_src !== undefined) {
       this.renderer.setProperty(this.input.nativeElement, 'src', enterprise.image_src);
+    }
+
+    if (this.page_client_view !== undefined && this.page_client_view === true) {
+      this.renderer.setStyle(this.input2.nativeElement, 'display', 'none');
+      this.renderer.setStyle(this.input3.nativeElement, 'display', 'none');
     }
   }
 
